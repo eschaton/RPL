0fe81f245c9969e4755c095148053bdd4cf1ff25
Chris Hanson <cmhanson@eschatologist.net>
2026-09-05 21:25:23 -0700

    Move BNF to tokenizer header

--- librpl/execution/rpl_tokenizer.c
+++ librpl/execution/rpl_tokenizer.c
@@ -19,42 +19,6 @@
 RPL_SOURCE_BEGIN
 
 
-/*
- RPL syntax is straightforward:
-
- integer := '#' /[0-9A-Fa-f]+[bdoh]?/.
- real := /[+-]?[0-9]*(.[0-9]*)?([eE][+-][0-9]+)?/.
- complex := '(' real ',' /[∡∢]?/ real ')'.
- array_element := real | complex | array.
- array := '[' array_element* ']'.
- name_text := character+.
- name := '\'' name_text '\''.
- program := '«' token+ '»'.
- string := '"' character* '"'.
- list := '{' value* '}'.
- tagged := ':' name_text ':' value.
- unit := real '_' name_text.
- value := integer | real | complex | array | name | program
-	| string | list | tagged | unit.
- identifier := name_text.
- comment := '@' character* end_of_line.
-
- Almost all syntactic units in the grammar above are separable by
- whitespace other than the following:
-
- - @ by itself starts a comment that spans to the next end of line
- - name_text does not allow whitespace, ', or "
- - string requires backslash-quoting before " and \ and supports the
-   usual quoted C characters (\\, \t, \r, \n)
- - tagged does not allow whitespace between syntactic subunits
- - unit does not allow whitespace between syntactic subunits
- - array, program, and list support nesting
-
- Identifiers are used to represent operations and are immediately
- evaluated.
- */
-
-
 rpl_tokenizer_t RPL_NULLABLE
 rpl_tokenizer_new(rpl_context_t context)
 {
--- librpl/execution/rpl_tokenizer.h
+++ librpl/execution/rpl_tokenizer.h
@@ -41,7 +41,44 @@ RPL_EXPORT
 bool
 rpl_tokenizer_append(rpl_tokenizer_t tokenizer, const char *str);
 
-/*! Get the next token, if any, from the tokenizer. */
+/*!
+ Get the next token from the tokenizer.
+
+ RPL syntax is straightforward:
+
+     integer = '#' ' '? /[0-9A-Fa-f]+[bdho]?/.
+     real = /[+-]?[0-9]*(.[0-9]*)?([eE][+-][0-9]+)?/.
+     complex = '(' real ',' /[∡∢]?/ real ')'.
+     array_element = real | complex | array.
+     array = '[' array_element* ']'.
+     name_text = character+.
+     name = '\'' name_text '\''.
+     program = '«' token+ '»'.
+     string = '"' character* '"'.
+     list = '{' value* '}'.
+     tagged = ':' name_text ':' value.
+     unit = real '_' name_text.
+     value = integer | real | complex | array | name
+           | program | string | list | tagged | unit.
+     identifier = name_text.
+     comment = '@' character* end_of_line.
+
+ Almost all syntactic units in the grammar above are separable by
+ whitespace other than the following:
+
+ - binary integers allow (but don't require) exactly one space between
+   the initial `'#'` and the digit characters
+ - name_text does not allow whitespace or delimiters
+ - string requires backslash-quoting before `'"'` and `'\'` and supports
+   the usual quoted C characters (`'\\'`, `'\t'`, `'\r'`, `'\n'`)
+ - tagged does not allow whitespace between syntactic subunits
+ - unit does not allow whitespace between syntactic subunits
+ - array, program, and list support nesting
+ - `'@'` by itself starts a comment that spans to the next end of line
+
+ Identifiers are used to represent operations and are immediately
+ evaluated.
+ */
 RPL_EXPORT
 rpl_token_t RPL_NULLABLE
 rpl_tokenizer_get_next(rpl_tokenizer_t tokenizer);
